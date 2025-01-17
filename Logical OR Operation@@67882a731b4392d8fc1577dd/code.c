f3d73fd4c34f814d1834d733c320d19c24a75b6e
hemantjoshee <hemant1341.becse24@chitkara.edu.in>
2025-01-18 00:43:09 +0530

    Solution to Logical OR Operation

---
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    a||b>0?printf("True"):printf("False");
+
+    return 0;
+}
