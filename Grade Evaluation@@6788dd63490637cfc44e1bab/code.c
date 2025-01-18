7903140fbd0fc2edd5d286f881d4fd7701243976
hemantjoshee <hemant1341.becse24@chitkara.edu.in>
2025-01-19 00:53:45 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    char a;
+    scanf("%d",&a);
+    switch (a){
+        case 'A':printf("Excellent");
+        break;
+        case 'B':printf("Good");
+        break;
+        case 'C':printf("Average");
+        break;
+        case 'D':printf("Below Average");
+        break;
+        case 'E':printf("Fail");
+        break;
+        default:printf("Invalid grade");
+    }
+    return 0;
+}
