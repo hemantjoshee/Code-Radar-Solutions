f121aa56cd79fef64f0a0537d7256c91567baa82
hemantjoshee <hemant1341.becse24@chitkara.edu.in>
2025-03-07 18:30:41 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -1,48 +1,25 @@
 #include <limits.h>
-int findUnsortedSubarray(int arr[], int n) {
-    int l = 0, r = n - 1;
-    while (l < n - 1 && arr[l] <= arr[l + 1]) {
+int findUnsortedSubarray(int a[],int n){
+    int l = 0 ;
+    int r = n - 1 ;
+    while ( l < n - 1 && a[l] < a[l+1]){
         l++;
     }
-    if (l == n - 1) return 0;
-    while (r > 0 && arr[r] >= arr[r - 1]) {
+    while ( r>0 && a[r]>a[r-1]){
         r--;
     }
 
-    int min = INT_MAX;
-    int max = INT_MIN;
-    for(int i=l; i<=r; i++){
-        if(arr[i] > max) max = arr[i];
-        if(arr[i] < min) min = arr[i];
+    int min =INT_MAX;
+    int max=INT_MIN;
+    for(int i=l;i<r;i++){
+        if (a[i]>max) max=a[i];
+        if(a[i]<min) min=a[i];
     }
-
-    while(l>0 && arr[l-1] > min) l--;
-    while(r < n-1 && arr[r+1] < max ) r++;
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // int min_val = INT_MAX, max_val = INT_MIN;
-    // for (int i = l; i <= r; i++) {
-    //     if (arr[i] < min_val) min_val = arr[i];
-    //     if (arr[i] > max_val) max_val = arr[i];
-    // }
-    // while (l > 0 && arr[l - 1] > min_val) {
-    //     l--;
-    // }
-    // while (r < n - 1 && arr[r + 1] < max_val) {
-    //     r++;
-    // }
-    return r - l + 1;
+    while(l>0 && a[l-1]>min){
+        l--;
+    }
+    while(r<n-1 &&a[r+1]<max){
+        r++;
+    }
+    return r-l+1
 }
