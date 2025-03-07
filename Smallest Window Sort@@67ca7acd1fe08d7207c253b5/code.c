229b296835c754fb5fc4439579de6fdfbe2b10a9
hemantjoshee <hemant1341.becse24@chitkara.edu.in>
2025-03-07 15:02:43 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -1,30 +1,36 @@
+#include <limits.h>
+
+// Function to find the length of the smallest subarray to be sorted
 int findUnsortedSubarray(int arr[], int n) {
     int l = 0, r = n - 1;
 
-    // Find the left boundary
+    // Find the first element from left that is out of order
     while (l < n - 1 && arr[l] <= arr[l + 1]) {
         l++;
     }
-    
-    // If the whole array is already sorted
+
+    // If the entire array is sorted
     if (l == n - 1) return 0;
 
-    // Find the right boundary
+    // Find the first element from right that is out of order
     while (r > 0 && arr[r] >= arr[r - 1]) {
         r--;
     }
 
-    // Find min and max in the unsorted subarray
-    int minVal = *min_element(arr + l, arr + r + 1);
-    int maxVal = *max_element(arr + l, arr + r + 1);
+    // Find the min and max in the unsorted subarray
+    int min_val = INT_MAX, max_val = INT_MIN;
+    for (int i = l; i <= r; i++) {
+        if (arr[i] < min_val) min_val = arr[i];
+        if (arr[i] > max_val) max_val = arr[i];
+    }
 
     // Expand left boundary
-    while (l > 0 && arr[l - 1] > minVal) {
+    while (l > 0 && arr[l - 1] > min_val) {
         l--;
     }
 
     // Expand right boundary
-    while (r < n - 1 && arr[r + 1] < maxVal) {
+    while (r < n - 1 && arr[r + 1] < max_val) {
         r++;
     }
 
