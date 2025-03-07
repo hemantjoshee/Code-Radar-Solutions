0a4b674b305bb793196ba25a87b8cf24796e4005
hemantjoshee <hemant1341.becse24@chitkara.edu.in>
2025-03-07 15:15:45 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -8,16 +8,39 @@ int findUnsortedSubarray(int arr[], int n) {
     while (r > 0 && arr[r] >= arr[r - 1]) {
         r--;
     }
-    int min_val = INT_MAX, max_val = INT_MIN;
-    for (int i = l; i <= r; i++) {
-        if (arr[i] < min_val) min_val = arr[i];
-        if (arr[i] > max_val) max_val = arr[i];
+
+    int min = INT_MAX;
+    int max = INT_MIN;
+    for(int i=l; i<=r; i++){
+        if(arr[i] > max) max = arr[i];
+        if(arr[i] < min) min = arr[i];
     }
-    while (l > 0 && arr[l - 1] > min_val) {
-        l--;
-    }
-    while (r < n - 1 && arr[r + 1] < max_val) {
-        r++;
-    }
+
+    while(l>0 && arr[l-1] > min) l--;
+    while(r < n && arr[r+1] > max ) r++;
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // int min_val = INT_MAX, max_val = INT_MIN;
+    // for (int i = l; i <= r; i++) {
+    //     if (arr[i] < min_val) min_val = arr[i];
+    //     if (arr[i] > max_val) max_val = arr[i];
+    // }
+    // while (l > 0 && arr[l - 1] > min_val) {
+    //     l--;
+    // }
+    // while (r < n - 1 && arr[r + 1] < max_val) {
+    //     r++;
+    // }
     return r - l + 1;
 }
